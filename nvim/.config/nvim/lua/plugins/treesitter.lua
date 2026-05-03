6848a6ed1dbbfaaaf950634d42b0321c855d75b1
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2026-05-03 02:05:59 -0400

    feat: switch to main branch

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,97 +1,56 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
 		lazy = false,
-		branch = "master",
 		build = ":TSUpdate",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				modules = {},
-				ignore_install = {},
-				ensure_installed = {
-					"bash",
-					"javascript",
-					"typescript",
-					"tsx",
-					"lua",
-					"markdown",
-					"markdown_inline",
-					"regex",
-					"svelte",
-					"vue",
-					"html",
-					"json",
-					"jsonc",
-					"jsdoc",
-					"yaml",
-					"toml",
-					"xml",
-					"graphql",
-					"astro",
-					"css",
-					"scss",
-				},
-				sync_install = false,
-				auto_install = false,
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-				indent = {
-					enable = true,
-				},
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<C-Space>",
-						node_incremental = "<C-Space>",
-						scope_incremental = false,
-						node_decremental = "<BS>",
-					},
-				},
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-							["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
-						},
-						selection_modes = {
-							["@parameter.outer"] = "v",
-							["@function.outer"] = "V",
-							["@class.outer"] = "<c-v>",
-						},
-						include_surrounding_whitespace = true,
-					},
-				},
-			})
+			local ts = require("nvim-treesitter")
+			local languages = {
+				"bash",
+				"javascript",
+				"typescript",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"regex",
+				"svelte",
+				"html",
+				"json",
+				"jsdoc",
+				"yaml",
+				"toml",
+				"xml",
+				"css",
+				"zsh",
+			}
 
-			vim.filetype.add({
-				extension = {
-					mdx = "mdx",
-				},
+			for _, language in ipairs(languages) do
+				ts.install(language)
+			end
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = languages,
+				callback = function()
+					vim.treesitter.start()
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
 			})
 
 			vim.treesitter.language.register("bash", "zsh")
 			vim.treesitter.language.register("markdown", "mdx")
+
+			vim.keymap.set("x", "<C-Space>", function()
+				require("vim.treesitter._select").select_parent(vim.v.count1)
+			end)
+			vim.keymap.set("x", "<BS>", function()
+				require("vim.treesitter._select").select_child(vim.v.count1)
+			end)
 		end,
 	},
 	{
 		"windwp/nvim-ts-autotag",
 		events = { "BufReadPre", "BufNewFile" },
-		opts = {
-			opts = {
-				enable_close = true,
-				enable_rename = true,
-				enable_close_on_slash = false,
-			},
-		},
+		opts = {},
 	},
 }
