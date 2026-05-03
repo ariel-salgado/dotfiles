6c2678f5d8a45112336fb65c6ea0d6f84c1be338
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2026-05-03 02:12:49 -0400

    fix: correctly using eslint fix as formatting option

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -2,35 +2,46 @@ return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
+	---@module "conform"
+	---@type conform.setupOpts
+	opts = {
+		format_on_save = function(bufnr)
+			local ft = vim.bo[bufnr].filetype
+			if vim.tbl_contains({ "javascript", "typescript", "svelte" }, ft) then
+				local ok = pcall(function()
+					vim.cmd("LspEslintFixAll")
+				end)
+				if not ok then
+					require("conform").format({ bufnr = bufnr, lsp_format = "fallback", timeout_ms = 500 })
+				end
+				return
+			end
+			return { timeout_ms = 500, lsp_format = "fallback" }
+		end,
+		formatters_by_ft = {
+			lua = { "stylua" },
+		},
+	},
 	keys = {
 		{
 			"<leader>fm",
 			function()
-				require("conform").format({
-					async = true,
-					lsp_fallback = true,
-				})
+				vim.schedule(function()
+					local ft = vim.bo.filetype
+					if vim.tbl_contains({ "javascript", "typescript", "svelte" }, ft) then
+						local ok = pcall(function()
+							vim.cmd("LspEslintFixAll")
+						end)
+						if not ok then
+							require("conform").format({ async = true, lsp_format = "fallback" })
+						end
+					else
+						require("conform").format({ async = true, lsp_format = "fallback" })
+					end
+				end)
 			end,
-			mode = "",
-			desc = "[F]ormat buffer",
-		},
-	},
-	---@module "conform"
-	---@type conform.setupOpts
-	opts = {
-		notify_on_error = false,
-		format_on_save = function(bufnr)
-			local disable_filetypes = {}
-			return {
-				timeout_ms = 500,
-				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-			}
-		end,
-		formatters_by_ft = {
-			lua = { "stylua" },
-			javascript = { "eslint", "prettierd", "prettier", stop_after_first = true },
-			typescript = { "eslint", "prettierd", "prettier", stop_after_first = true },
-			svelte = { "eslint", "prettierd", "prettier", stop_after_first = true },
+			mode = { "n", "v" },
+			desc = "Format buffer or range",
 		},
 	},
 }
